480083fccb9182c356f62354b07c77d879d22e9e
Agustin Bustos Piasentini <bustos-agustin@hotmail.com - agustin.bustos.p@gmail.com>
2023-02-19 21:07:36 -0300

    Primeros pasos de la modularización

--- Ajuste Art 13.py
+++ Ajuste Art 13.py
@@ -1,94 +1,105 @@
 import os , pandas as pd , numpy as np , openpyxl
+from tkinter import filedialog
 
-Objetivo = "Anual/"
+def Ajuste_Art13_XLS():
 
-# Listar los archivos de la carpeta 'Anual'
-archivos = os.listdir ( Objetivo )
+    Objetivo = filedialog.askdirectory(title = "Seleccionar carpeta de archivos") + "/"
 
-#crear un dataframe vacio
-Consolidado = pd.DataFrame()
+    if Objetivo != "/":
 
-#consolidar los archivos Excel en un solo dataframe
-for i in archivos:
-    data = pd.read_html( (Objetivo + i) , decimal="," , thousands="" , header = 0 ,)[0]
-    #Crear una columna con el nombre del archivo
-    data['Archivo'] = i
-    Consolidado = pd.concat([Consolidado , data])
-del data , i , archivos , Objetivo
+        # Listar los archivos de la carpeta 'Anual'
+        archivos = os.listdir ( Objetivo )
 
-#Eliminar columnas 'id' , 'idcuit' , 'idtipo_operacion' , 'idprovinciaiibb' , 'provincia' , 'idcentrocosto' , 'idcomprobanteorigina' , 'idclipro' , 'idcliprodomicilio' , 'idmodelo' , 'modelo' , 'codactividad' , 'numerohasta' , 'CAE_obtener' , 'CAE_intentos' , 'CAE_iderrorlog' , 'CAE_fecha' , 'CAE_numero' , 'CAE_fechavencimiento' , 'CAE_importetotal' , 'CAE_testing' , 'CAE_fechahorapedido' , 'CAE_fechahoraobtenido' , 'CAE_idusuariomodi' , 'FE_url' , 'FE_fechahoramodi' , 'FE_idusuariomodi' , 'FE_fechahoraenvio' , 'FE_idusuarioenvio' , 'memo_factura' , 'memo_remito' , 'memo_cheque' , 'idcuenta' , 'fechaalta' , 'idusuarioalta' , 'usuarioalta' , 'fechamodi' , 'idusuariomodi' , 'usuariomodi' , 'fechabaja' , 'idusuariobaja' , 'usuariobaja' , 'tipoespecial' , 'idcomprobanteventa' , 'identificador' de consolidado
-Consolidado = Consolidado.drop(['id' , 'idcuit' , 'idtipo_operacion' , 'idprovinciaiibb' , 'provincia' , 'idcentrocosto' , 'idcomprobanteorigina' , 'idclipro' , 'idcliprodomicilio' , 'idmodelo' , 'modelo' , 'codactividad' , 'numerohasta' , 'CAE_obtener' , 'CAE_intentos' , 'CAE_iderrorlog' , 'CAE_fecha' , 'CAE_numero' , 'CAE_fechavencimiento' , 'CAE_importetotal' , 'CAE_testing' , 'CAE_fechahorapedido' , 'CAE_fechahoraobtenido' , 'CAE_idusuariomodi' , 'FE_url' , 'FE_fechahoramodi' , 'FE_idusuariomodi' , 'FE_fechahoraenvio' , 'FE_idusuarioenvio' , 'memo_factura' , 'memo_remito' , 'memo_cheque' , 'idcuenta' , 'fechaalta' , 'idusuarioalta' , 'usuarioalta' , 'fechamodi' , 'idusuariomodi' , 'usuariomodi' , 'fechabaja' , 'idusuariobaja' , 'usuariobaja' , 'tipoespecial' , 'idcomprobanteventa' , 'identificador'] , axis = 1)
+        # Eliminar los archivos que no sean .csv
+        archivos = [i for i in archivos if i.endswith(".csv")]
 
-#Rellenar las columnas 'montoneto' , 'montonogravado' , 'montoexento' , 'montoiva' , 'montoperc' , 'montoperc_nac' , 'montoperc_iibb' , 'montoimpmunic' , 'montoimpint' , 'montootrostrib' , 'montototal' con 0
-Consolidado['montoneto'] = Consolidado['montoneto'].fillna(0)
-Consolidado['montonogravado'] = Consolidado['montonogravado'].fillna(0)
-Consolidado['montoexento'] = Consolidado['montoexento'].fillna(0)
-Consolidado['montoiva'] = Consolidado['montoiva'].fillna(0)
-Consolidado['montoperc'] = Consolidado['montoperc'].fillna(0)
-Consolidado['montoperc_nac'] = Consolidado['montoperc_nac'].fillna(0)
-Consolidado['montoperc_iibb'] = Consolidado['montoperc_iibb'].fillna(0)
-Consolidado['montoimpmunic'] = Consolidado['montoimpmunic'].fillna(0)
-Consolidado['montoimpint'] = Consolidado['montoimpint'].fillna(0)
-Consolidado['montootrostrib'] = Consolidado['montootrostrib'].fillna(0)
-Consolidado['montototal'] = Consolidado['montototal'].fillna(0)
+        #crear un dataframe vacio
+        Consolidado = pd.DataFrame()
 
+        #consolidar los archivos Excel en un solo dataframe
+        for i in archivos:
+            data = pd.read_html( (Objetivo + i) , decimal="," , thousands="" , header = 0 ,)[0]
+            #Crear una columna con el nombre del archivo
+            data['Archivo'] = i
+            Consolidado = pd.concat([Consolidado , data])
+        del data , i , archivos
 
-#Crear columna de 'CLIENTE Art 13' teniendo en cuenta los caracteres de la columna 'Archivo' desde la posición 15 hasta el final (sin tener el cuenta' - Anual 2022.xlsx')
-Consolidado['CLIENTE Art 13'] = Consolidado['Archivo'].str.slice(15, -18)
+        #Eliminar columnas 'id' , 'idcuit' , 'idtipo_operacion' , 'idprovinciaiibb' , 'provincia' , 'idcentrocosto' , 'idcomprobanteorigina' , 'idclipro' , 'idcliprodomicilio' , 'idmodelo' , 'modelo' , 'codactividad' , 'numerohasta' , 'CAE_obtener' , 'CAE_intentos' , 'CAE_iderrorlog' , 'CAE_fecha' , 'CAE_numero' , 'CAE_fechavencimiento' , 'CAE_importetotal' , 'CAE_testing' , 'CAE_fechahorapedido' , 'CAE_fechahoraobtenido' , 'CAE_idusuariomodi' , 'FE_url' , 'FE_fechahoramodi' , 'FE_idusuariomodi' , 'FE_fechahoraenvio' , 'FE_idusuarioenvio' , 'memo_factura' , 'memo_remito' , 'memo_cheque' , 'idcuenta' , 'fechaalta' , 'idusuarioalta' , 'usuarioalta' , 'fechamodi' , 'idusuariomodi' , 'usuariomodi' , 'fechabaja' , 'idusuariobaja' , 'usuariobaja' , 'tipoespecial' , 'idcomprobanteventa' , 'identificador' de consolidado
+        Consolidado = Consolidado.drop(['id' , 'idcuit' , 'idtipo_operacion' , 'idprovinciaiibb' , 'provincia' , 'idcentrocosto' , 'idcomprobanteorigina' , 'idclipro' , 'idcliprodomicilio' , 'idmodelo' , 'modelo' , 'codactividad' , 'numerohasta' , 'CAE_obtener' , 'CAE_intentos' , 'CAE_iderrorlog' , 'CAE_fecha' , 'CAE_numero' , 'CAE_fechavencimiento' , 'CAE_importetotal' , 'CAE_testing' , 'CAE_fechahorapedido' , 'CAE_fechahoraobtenido' , 'CAE_idusuariomodi' , 'FE_url' , 'FE_fechahoramodi' , 'FE_idusuariomodi' , 'FE_fechahoraenvio' , 'FE_idusuarioenvio' , 'memo_factura' , 'memo_remito' , 'memo_cheque' , 'idcuenta' , 'fechaalta' , 'idusuarioalta' , 'usuarioalta' , 'fechamodi' , 'idusuariomodi' , 'usuariomodi' , 'fechabaja' , 'idusuariobaja' , 'usuariobaja' , 'tipoespecial' , 'idcomprobanteventa' , 'identificador'] , axis = 1)
 
-#crear columna de periodo en base a la columna 'fechaiva' (primero pasarla a formato datetime DD/MM/AAAA) y convertirla a formato AAAAMM
-Consolidado['periodo'] = pd.to_datetime(Consolidado['fechaiva'], format='%d/%m/%Y').dt.strftime('%Y%m')
-
-#Crear columnas auxiliares
-Consolidado['NG'] = Consolidado['montonogravado'] + Consolidado['montoexento']
-Consolidado['Neto + NG'] = Consolidado['montoneto'] + Consolidado['montonogravado'] + Consolidado['montoexento']
-
-#Crear dataframe con 'Compras' y 'Ventas' en la columna 'tipooperacion'
-Compras = Consolidado[Consolidado['tipooperacion'] == 'Compras']
-Ventas = Consolidado[Consolidado['tipooperacion'] == 'Ventas']
+        #Rellenar las columnas 'montoneto' , 'montonogravado' , 'montoexento' , 'montoiva' , 'montoperc' , 'montoperc_nac' , 'montoperc_iibb' , 'montoimpmunic' , 'montoimpint' , 'montootrostrib' , 'montototal' con 0
+        Consolidado['montoneto'] = Consolidado['montoneto'].fillna(0)
+        Consolidado['montonogravado'] = Consolidado['montonogravado'].fillna(0)
+        Consolidado['montoexento'] = Consolidado['montoexento'].fillna(0)
+        Consolidado['montoiva'] = Consolidado['montoiva'].fillna(0)
+        Consolidado['montoperc'] = Consolidado['montoperc'].fillna(0)
+        Consolidado['montoperc_nac'] = Consolidado['montoperc_nac'].fillna(0)
+        Consolidado['montoperc_iibb'] = Consolidado['montoperc_iibb'].fillna(0)
+        Consolidado['montoimpmunic'] = Consolidado['montoimpmunic'].fillna(0)
+        Consolidado['montoimpint'] = Consolidado['montoimpint'].fillna(0)
+        Consolidado['montootrostrib'] = Consolidado['montootrostrib'].fillna(0)
+        Consolidado['montototal'] = Consolidado['montototal'].fillna(0)
 
 
-#Crear tablas pivote de 'Compras' y 'Ventas' por 'CLIENTE Art 13' y 'periodo'
-Compras_TD = Compras.pivot_table(index = ['CLIENTE Art 13' , 'periodo'] , values = ['montoneto' , 'NG' , 'Neto + NG' , 'montoiva' ] , aggfunc = 'sum')
-Ventas_TD = Ventas.pivot_table(index = ['CLIENTE Art 13' , 'periodo'] , values = ['montoneto' , 'NG' , 'Neto + NG'] , aggfunc = 'sum')
-Ventas_Computable = Ventas.pivot_table(index = ['CLIENTE Art 13'] , values = ['montoneto' , 'Neto + NG'] , aggfunc = 'sum')
+        #Crear columna de 'CLIENTE Art 13' teniendo en cuenta los caracteres de la columna 'Archivo' desde la posición 15 hasta el final (sin tener el cuenta' - Anual 2022.xlsx')
+        Consolidado['CLIENTE Art 13'] = Consolidado['Archivo'].str.slice(15, -18)
 
-#Crear columna de '% Computable' en base a la columna 'montoneto' / 'Neto + NG' de la tabla 'Ventas' y 'Ventas_Computable'
-Ventas_TD['% Computado'] = Ventas_TD['montoneto'] / Ventas_TD['Neto + NG']
-Ventas_Computable['% Computable'] = Ventas_Computable['montoneto'] / Ventas_Computable['Neto + NG']
+        #crear columna de periodo en base a la columna 'fechaiva' (primero pasarla a formato datetime DD/MM/AAAA) y convertirla a formato AAAAMM
+        Consolidado['periodo'] = pd.to_datetime(Consolidado['fechaiva'], format='%d/%m/%Y').dt.strftime('%Y%m')
 
-#Separar el indice de las tablas pivote en columnas
-Compras_TD.reset_index(inplace=True)
-Ventas_TD.reset_index(inplace=True)
-Ventas_Computable.reset_index(inplace=True)
+        #Crear columnas auxiliares
+        Consolidado['NG'] = Consolidado['montonogravado'] + Consolidado['montoexento']
+        Consolidado['Neto + NG'] = Consolidado['montoneto'] + Consolidado['montonogravado'] + Consolidado['montoexento']
 
-#crear columas AUX en Compas_TD y Ventas_TD
-Compras_TD['Aux'] = Compras_TD['CLIENTE Art 13'] + ' - '+ Compras_TD['periodo'].astype(str)
-Ventas_TD['Aux'] = Ventas_TD['CLIENTE Art 13'] + ' - ' + Ventas_TD['periodo'].astype(str)
+        #Crear dataframe con 'Compras' y 'Ventas' en la columna 'tipooperacion'
+        Compras = Consolidado[Consolidado['tipooperacion'] == 'Compras']
+        Ventas = Consolidado[Consolidado['tipooperacion'] == 'Ventas']
 
-#Merge de Compras_TD y Ventas_TD en base a la columna 'Aux' y traer solamente la columna '% Computado' de Ventas_TD
-Compras_TD = pd.merge(  Compras_TD,
-                        Ventas_TD[['Aux', '% Computado']],
-                        left_on='Aux' ,
-                        right_on='Aux',
-                        how='left')
 
-#merge de Compras_TD y Ventas_Computable en base a la columna 'CLIENTE Art 13' y traer solamente la columna '% Computable' de Ventas_Computable
-Compras_TD = pd.merge(  Compras_TD,
-                        Ventas_Computable[['CLIENTE Art 13', '% Computable']],
-                        left_on='CLIENTE Art 13' ,
-                        right_on='CLIENTE Art 13',
-                        how='left')
+        #Crear tablas pivote de 'Compras' y 'Ventas' por 'CLIENTE Art 13' y 'periodo'
+        Compras_TD = Compras.pivot_table(index = ['CLIENTE Art 13' , 'periodo'] , values = ['montoneto' , 'NG' , 'Neto + NG' , 'montoiva' ] , aggfunc = 'sum')
+        Ventas_TD = Ventas.pivot_table(index = ['CLIENTE Art 13' , 'periodo'] , values = ['montoneto' , 'NG' , 'Neto + NG'] , aggfunc = 'sum')
+        Ventas_Computable = Ventas.pivot_table(index = ['CLIENTE Art 13'] , values = ['montoneto' , 'Neto + NG'] , aggfunc = 'sum')
 
-#Crear columna de diferencia entre '% Computado' y '% Computable' y multiplicarlo por el 'montoiva'
-Compras_TD['Diferencia'] = (Compras_TD['% Computable'] - Compras_TD['% Computado']) * Compras_TD['montoiva']
+        #Crear columna de '% Computable' en base a la columna 'montoneto' / 'Neto + NG' de la tabla 'Ventas' y 'Ventas_Computable'
+        Ventas_TD['% Computado'] = Ventas_TD['montoneto'] / Ventas_TD['Neto + NG']
+        Ventas_Computable['% Computable'] = Ventas_Computable['montoneto'] / Ventas_Computable['Neto + NG']
 
-#Crear pivot table de 'Diferencia' por 'CLIENTE Art 13'
-Diferencia = Compras_TD.pivot_table(index = ['CLIENTE Art 13'] , values = ['Diferencia'] , aggfunc = 'sum')
+        #Separar el indice de las tablas pivote en columnas
+        Compras_TD.reset_index(inplace=True)
+        Ventas_TD.reset_index(inplace=True)
+        Ventas_Computable.reset_index(inplace=True)
 
-#exportar las tablas pivote a excel en solapas distintas
-with pd.ExcelWriter('Consolidado.xlsx') as writer:
-    Compras_TD.to_excel(writer, sheet_name='Compras', index=False)
-    Ventas_TD.to_excel(writer, sheet_name='Ventas' , index=False)
-    Ventas_Computable.to_excel(writer, sheet_name='Ventas_Computable' , index=False)
-    Diferencia.to_excel(writer, sheet_name='Resumen' , index=True)
+        #crear columas AUX en Compas_TD y Ventas_TD
+        Compras_TD['Aux'] = Compras_TD['CLIENTE Art 13'] + ' - '+ Compras_TD['periodo'].astype(str)
+        Ventas_TD['Aux'] = Ventas_TD['CLIENTE Art 13'] + ' - ' + Ventas_TD['periodo'].astype(str)
+
+        #Merge de Compras_TD y Ventas_TD en base a la columna 'Aux' y traer solamente la columna '% Computado' de Ventas_TD
+        Compras_TD = pd.merge(  Compras_TD,
+                                Ventas_TD[['Aux', '% Computado']],
+                                left_on='Aux' ,
+                                right_on='Aux',
+                                how='left')
+
+        #merge de Compras_TD y Ventas_Computable en base a la columna 'CLIENTE Art 13' y traer solamente la columna '% Computable' de Ventas_Computable
+        Compras_TD = pd.merge(  Compras_TD,
+                                Ventas_Computable[['CLIENTE Art 13', '% Computable']],
+                                left_on='CLIENTE Art 13' ,
+                                right_on='CLIENTE Art 13',
+                                how='left')
+
+        #Crear columna de diferencia entre '% Computado' y '% Computable' y multiplicarlo por el 'montoiva'
+        Compras_TD['Diferencia'] = (Compras_TD['% Computable'] - Compras_TD['% Computado']) * Compras_TD['montoiva']
+
+        #Crear pivot table de 'Diferencia' por 'CLIENTE Art 13'
+        Diferencia = Compras_TD.pivot_table(index = ['CLIENTE Art 13'] , values = ['Diferencia'] , aggfunc = 'sum')
+
+        #exportar las tablas pivote a excel en solapas distintas
+        with pd.ExcelWriter(f'{Objetivo}Consolidado.xlsx') as writer:
+            Compras_TD.to_excel(writer, sheet_name='Compras', index=False)
+            Ventas_TD.to_excel(writer, sheet_name='Ventas' , index=False)
+            Ventas_Computable.to_excel(writer, sheet_name='Ventas_Computable' , index=False)
+            Diferencia.to_excel(writer, sheet_name='Resumen' , index=True)
+
+if __name__ == '__main__':
+    Ajuste_Art13_XLS()
--- Prorrateo mensual.py
+++ Prorrateo mensual.py
@@ -1,82 +1,89 @@
 import os , pandas as pd , numpy as np , openpyxl
 from tkinter import filedialog
 
-Objetivo = filedialog.askdirectory(title = "Seleccionar carpeta de archivos") + "/"
+def Prorrateo_Mensual_XLS():
 
-# Listar los archivos de la carpeta seleccionada
-archivos = os.listdir ( Objetivo )
+    Objetivo = filedialog.askdirectory(title = "Seleccionar carpeta de archivos") + "/"
 
-# Eliminar los archivos que no sean .csv
-archivos = [i for i in archivos if i.endswith(".xls")]
+    if Objetivo != "/":
 
-#crear un dataframe vacio
-Consolidado = pd.DataFrame()
+        # Listar los archivos de la carpeta seleccionada
+        archivos = os.listdir ( Objetivo )
 
-#consolidar los archivos Excel en un solo dataframe
-for i in archivos:
-    data = pd.read_html( (Objetivo + i) , decimal="," , thousands="" , header = 0 ,)[0]
-    #Crear una columna con el nombre del archivo
-    data['Archivo'] = i
-    Consolidado = pd.concat([Consolidado , data])
-del data , i , archivos
+        # Eliminar los archivos que no sean .xls
+        archivos = [i for i in archivos if i.endswith(".xls")]
 
-#Eliminar columnas 'id' , 'idcuit' , 'idtipo_operacion' , 'idprovinciaiibb' , 'provincia' , 'idcentrocosto' , 'idcomprobanteorigina' , 'idclipro' , 'idcliprodomicilio' , 'idmodelo' , 'modelo' , 'codactividad' , 'numerohasta' , 'CAE_obtener' , 'CAE_intentos' , 'CAE_iderrorlog' , 'CAE_fecha' , 'CAE_numero' , 'CAE_fechavencimiento' , 'CAE_importetotal' , 'CAE_testing' , 'CAE_fechahorapedido' , 'CAE_fechahoraobtenido' , 'CAE_idusuariomodi' , 'FE_url' , 'FE_fechahoramodi' , 'FE_idusuariomodi' , 'FE_fechahoraenvio' , 'FE_idusuarioenvio' , 'memo_factura' , 'memo_remito' , 'memo_cheque' , 'idcuenta' , 'fechaalta' , 'idusuarioalta' , 'usuarioalta' , 'fechamodi' , 'idusuariomodi' , 'usuariomodi' , 'fechabaja' , 'idusuariobaja' , 'usuariobaja' , 'tipoespecial' , 'idcomprobanteventa' , 'identificador' de consolidado
-Consolidado = Consolidado.drop(['id' , 'idcuit' , 'idtipo_operacion' , 'idprovinciaiibb' , 'provincia' , 'idcentrocosto' , 'idcomprobanteorigina' , 'idclipro' , 'idcliprodomicilio' , 'idmodelo' , 'modelo' , 'codactividad' , 'numerohasta' , 'CAE_obtener' , 'CAE_intentos' , 'CAE_iderrorlog' , 'CAE_fecha' , 'CAE_numero' , 'CAE_fechavencimiento' , 'CAE_importetotal' , 'CAE_testing' , 'CAE_fechahorapedido' , 'CAE_fechahoraobtenido' , 'CAE_idusuariomodi' , 'FE_url' , 'FE_fechahoramodi' , 'FE_idusuariomodi' , 'FE_fechahoraenvio' , 'FE_idusuarioenvio' , 'memo_factura' , 'memo_remito' , 'memo_cheque' , 'idcuenta' , 'fechaalta' , 'idusuarioalta' , 'usuarioalta' , 'fechamodi' , 'idusuariomodi' , 'usuariomodi' , 'fechabaja' , 'idusuariobaja' , 'usuariobaja' , 'tipoespecial' , 'idcomprobanteventa' , 'identificador'] , axis = 1)
+        #crear un dataframe vacio
+        Consolidado = pd.DataFrame()
 
-#Rellenar las columnas 'montoneto' , 'montonogravado' , 'montoexento' , 'montoiva' , 'montoperc' , 'montoperc_nac' , 'montoperc_iibb' , 'montoimpmunic' , 'montoimpint' , 'montootrostrib' , 'montototal' con 0
-Consolidado['montoneto'] = Consolidado['montoneto'].fillna(0)
-Consolidado['montonogravado'] = Consolidado['montonogravado'].fillna(0)
-Consolidado['montoexento'] = Consolidado['montoexento'].fillna(0)
-Consolidado['montoiva'] = Consolidado['montoiva'].fillna(0)
-Consolidado['montoperc'] = Consolidado['montoperc'].fillna(0)
-Consolidado['montoperc_nac'] = Consolidado['montoperc_nac'].fillna(0)
-Consolidado['montoperc_iibb'] = Consolidado['montoperc_iibb'].fillna(0)
-Consolidado['montoimpmunic'] = Consolidado['montoimpmunic'].fillna(0)
-Consolidado['montoimpint'] = Consolidado['montoimpint'].fillna(0)
-Consolidado['montootrostrib'] = Consolidado['montootrostrib'].fillna(0)
-Consolidado['montototal'] = Consolidado['montototal'].fillna(0)
+        #consolidar los archivos Excel en un solo dataframe
+        for i in archivos:
+            data = pd.read_html( (Objetivo + i) , decimal="," , thousands="" , header = 0 ,)[0]
+            #Crear una columna con el nombre del archivo
+            data['Archivo'] = i
+            Consolidado = pd.concat([Consolidado , data])
+        del data , i , archivos
 
-#Crear columna de 'CLIENTE' teniendo en cuenta los caracteres de la columna 'Archivo' desde la posición 15 hasta el final (sin tener el cuenta' - Anual 2022.xlsx')
-Consolidado['CLIENTE'] = Consolidado['Archivo'].str.slice(15, -18)
+        #Eliminar columnas 'id' , 'idcuit' , 'idtipo_operacion' , 'idprovinciaiibb' , 'provincia' , 'idcentrocosto' , 'idcomprobanteorigina' , 'idclipro' , 'idcliprodomicilio' , 'idmodelo' , 'modelo' , 'codactividad' , 'numerohasta' , 'CAE_obtener' , 'CAE_intentos' , 'CAE_iderrorlog' , 'CAE_fecha' , 'CAE_numero' , 'CAE_fechavencimiento' , 'CAE_importetotal' , 'CAE_testing' , 'CAE_fechahorapedido' , 'CAE_fechahoraobtenido' , 'CAE_idusuariomodi' , 'FE_url' , 'FE_fechahoramodi' , 'FE_idusuariomodi' , 'FE_fechahoraenvio' , 'FE_idusuarioenvio' , 'memo_factura' , 'memo_remito' , 'memo_cheque' , 'idcuenta' , 'fechaalta' , 'idusuarioalta' , 'usuarioalta' , 'fechamodi' , 'idusuariomodi' , 'usuariomodi' , 'fechabaja' , 'idusuariobaja' , 'usuariobaja' , 'tipoespecial' , 'idcomprobanteventa' , 'identificador' de consolidado
+        Consolidado = Consolidado.drop(['id' , 'idcuit' , 'idtipo_operacion' , 'idprovinciaiibb' , 'provincia' , 'idcentrocosto' , 'idcomprobanteorigina' , 'idclipro' , 'idcliprodomicilio' , 'idmodelo' , 'modelo' , 'codactividad' , 'numerohasta' , 'CAE_obtener' , 'CAE_intentos' , 'CAE_iderrorlog' , 'CAE_fecha' , 'CAE_numero' , 'CAE_fechavencimiento' , 'CAE_importetotal' , 'CAE_testing' , 'CAE_fechahorapedido' , 'CAE_fechahoraobtenido' , 'CAE_idusuariomodi' , 'FE_url' , 'FE_fechahoramodi' , 'FE_idusuariomodi' , 'FE_fechahoraenvio' , 'FE_idusuarioenvio' , 'memo_factura' , 'memo_remito' , 'memo_cheque' , 'idcuenta' , 'fechaalta' , 'idusuarioalta' , 'usuarioalta' , 'fechamodi' , 'idusuariomodi' , 'usuariomodi' , 'fechabaja' , 'idusuariobaja' , 'usuariobaja' , 'tipoespecial' , 'idcomprobanteventa' , 'identificador'] , axis = 1)
 
-#crear columna de periodo en base a la columna 'fechaiva' (primero pasarla a formato datetime DD/MM/AAAA) y convertirla a formato AAAAMM
-Consolidado['periodo'] = pd.to_datetime(Consolidado['fechaiva'], format='%d/%m/%Y').dt.strftime('%Y%m')
+        #Rellenar las columnas 'montoneto' , 'montonogravado' , 'montoexento' , 'montoiva' , 'montoperc' , 'montoperc_nac' , 'montoperc_iibb' , 'montoimpmunic' , 'montoimpint' , 'montootrostrib' , 'montototal' con 0
+        Consolidado['montoneto'] = Consolidado['montoneto'].fillna(0)
+        Consolidado['montonogravado'] = Consolidado['montonogravado'].fillna(0)
+        Consolidado['montoexento'] = Consolidado['montoexento'].fillna(0)
+        Consolidado['montoiva'] = Consolidado['montoiva'].fillna(0)
+        Consolidado['montoperc'] = Consolidado['montoperc'].fillna(0)
+        Consolidado['montoperc_nac'] = Consolidado['montoperc_nac'].fillna(0)
+        Consolidado['montoperc_iibb'] = Consolidado['montoperc_iibb'].fillna(0)
+        Consolidado['montoimpmunic'] = Consolidado['montoimpmunic'].fillna(0)
+        Consolidado['montoimpint'] = Consolidado['montoimpint'].fillna(0)
+        Consolidado['montootrostrib'] = Consolidado['montootrostrib'].fillna(0)
+        Consolidado['montototal'] = Consolidado['montototal'].fillna(0)
 
-#Crear columnas auxiliares
-Consolidado['NG'] = Consolidado['montonogravado'] + Consolidado['montoexento']
-Consolidado['Neto + NG'] = Consolidado['montoneto'] + Consolidado['montonogravado'] + Consolidado['montoexento']
+        #Crear columna de 'CLIENTE' teniendo en cuenta los caracteres de la columna 'Archivo' desde la posición 15 hasta el final (sin tener el cuenta' - Anual 2022.xlsx')
+        Consolidado['CLIENTE'] = Consolidado['Archivo'].str.slice(15, -18)
 
-#Crear dataframe con 'Compras' y 'Ventas' en la columna 'tipooperacion'
-Compras = Consolidado[Consolidado['tipooperacion'] == 'Compras']
-Ventas = Consolidado[Consolidado['tipooperacion'] == 'Ventas']
+        #crear columna de periodo en base a la columna 'fechaiva' (primero pasarla a formato datetime DD/MM/AAAA) y convertirla a formato AAAAMM
+        Consolidado['periodo'] = pd.to_datetime(Consolidado['fechaiva'], format='%d/%m/%Y').dt.strftime('%Y%m')
+
+        #Crear columnas auxiliares
+        Consolidado['NG'] = Consolidado['montonogravado'] + Consolidado['montoexento']
+        Consolidado['Neto + NG'] = Consolidado['montoneto'] + Consolidado['montonogravado'] + Consolidado['montoexento']
+
+        #Crear dataframe con 'Compras' y 'Ventas' en la columna 'tipooperacion'
+        Compras = Consolidado[Consolidado['tipooperacion'] == 'Compras']
+        Ventas = Consolidado[Consolidado['tipooperacion'] == 'Ventas']
 
 
-#Crear tablas pivote de 'Compras' y 'Ventas' por 'CLIENTE' y 'periodo'
-Compras_TD = Compras.pivot_table(index = ['CLIENTE' , 'periodo'] , values = ['montoneto' , 'NG' , 'Neto + NG' , 'montoiva' ] , aggfunc = 'sum')
-Ventas_TD = Ventas.pivot_table(index = ['CLIENTE' , 'periodo'] , values = ['montoneto' , 'NG' , 'Neto + NG'] , aggfunc = 'sum')
+        #Crear tablas pivote de 'Compras' y 'Ventas' por 'CLIENTE' y 'periodo'
+        Compras_TD = Compras.pivot_table(index = ['CLIENTE' , 'periodo'] , values = ['montoneto' , 'NG' , 'Neto + NG' , 'montoiva' ] , aggfunc = 'sum')
+        Ventas_TD = Ventas.pivot_table(index = ['CLIENTE' , 'periodo'] , values = ['montoneto' , 'NG' , 'Neto + NG'] , aggfunc = 'sum')
 
-#Crear columna de '% Computable' en base a la columna 'montoneto' / 'Neto + NG' de la tabla 'Ventas' y 'Ventas_Computable'
-Ventas_TD['% computable'] = Ventas_TD['montoneto'] / Ventas_TD['Neto + NG']
+        #Crear columna de '% Computable' en base a la columna 'montoneto' / 'Neto + NG' de la tabla 'Ventas' y 'Ventas_Computable'
+        Ventas_TD['% computable'] = Ventas_TD['montoneto'] / Ventas_TD['Neto + NG']
 
-#Separar el indice de las tablas pivote en columnas
-Compras_TD.reset_index(inplace=True)
-Ventas_TD.reset_index(inplace=True)
+        #Separar el indice de las tablas pivote en columnas
+        Compras_TD.reset_index(inplace=True)
+        Ventas_TD.reset_index(inplace=True)
 
-#crear columas AUX en Compas_TD y Ventas_TD
-Compras_TD['Aux'] = Compras_TD['CLIENTE'] + ' - '+ Compras_TD['periodo'].astype(str)
-Ventas_TD['Aux'] = Ventas_TD['CLIENTE'] + ' - ' + Ventas_TD['periodo'].astype(str)
+        #crear columas AUX en Compas_TD y Ventas_TD
+        Compras_TD['Aux'] = Compras_TD['CLIENTE'] + ' - '+ Compras_TD['periodo'].astype(str)
+        Ventas_TD['Aux'] = Ventas_TD['CLIENTE'] + ' - ' + Ventas_TD['periodo'].astype(str)
 
-#Merge de Compras_TD y Ventas_TD en base a la columna 'Aux' y traer solamente la columna '% computable' de Ventas_TD
-Compras_TD = pd.merge(  Compras_TD,
-                        Ventas_TD[['Aux', '% computable']],
-                        left_on='Aux' ,
-                        right_on='Aux',
-                        how='left')
+        #Merge de Compras_TD y Ventas_TD en base a la columna 'Aux' y traer solamente la columna '% computable' de Ventas_TD
+        Compras_TD = pd.merge(  Compras_TD,
+                                Ventas_TD[['Aux', '% computable']],
+                                left_on='Aux' ,
+                                right_on='Aux',
+                                how='left')
 
-#Crear columna de diferencia entre '% computable' y '% Computable' y multiplicarlo por el 'montoiva'
-Compras_TD['CF computable'] = Compras_TD['% computable'] * Compras_TD['montoiva']
+        #Crear columna de diferencia entre '% computable' y '% Computable' y multiplicarlo por el 'montoiva'
+        Compras_TD['CF computable'] = Compras_TD['% computable'] * Compras_TD['montoiva']
 
-#exportar las tablas pivote a excel en solapas distintas
-with pd.ExcelWriter( f'{Objetivo}Prorrateo mensual.xlsx') as writer:
-    Compras_TD.to_excel(writer, sheet_name='Compras', index=False)
-    Ventas_TD.to_excel(writer, sheet_name='Ventas' , index=False)
+        #exportar las tablas pivote a excel en solapas distintas
+        with pd.ExcelWriter( f'{Objetivo}Prorrateo mensual.xlsx') as writer:
+            Compras_TD.to_excel(writer, sheet_name='Compras', index=False)
+            Ventas_TD.to_excel(writer, sheet_name='Ventas' , index=False)
+
+if __name__ == '__main__':
+    Prorrateo_Mensual_XLS()
